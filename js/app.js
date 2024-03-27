36e8c448f22d7a22c46a2adf20eca6c7ae6ec430
MrSobhan <sobhan.mosazadeh85@gmail.com>
2024-03-27 22:52:35 +0330

    Add Git init

---
+++ js/app.js
@@ -0,0 +1,50 @@
+const $ = document;
+AOS.init();
+
+var app = document.getElementById('app');
+
+var typewriter = new Typewriter(app, {
+    loop: true
+});
+
+typewriter.typeString('من <سبحان /> هستم')
+    .pauseFor(2500)
+    .deleteChars(14)
+    .typeString('<فرانت اند دولوپر /> هستم')
+    .pauseFor(2500)
+    .deleteChars(26)
+    .typeString('<برنامه نویس /> هستم')
+    .pauseFor(2500)
+    .start();
+
+const btnDark = $.getElementById('dark')
+const btnLight = $.getElementById('light')
+
+window.addEventListener('load' , ()=>{
+    if(localStorage.getItem('them') == 'dark'){
+        $.documentElement.classList.add('dark')
+    }else{
+        $.documentElement.classList.remove('dark')
+    }
+})
+
+
+btnDark.addEventListener('click' , ()=>{
+    localStorage.setItem('them' , 'dark')
+    $.documentElement.classList.add('dark')
+})
+btnLight.addEventListener('click' , ()=>{
+    localStorage.setItem('them' , 'light')
+    $.documentElement.classList.remove('dark')
+})
+
+var swiper = new Swiper('.swiper-container', {
+    scrollbar: '.swiper-scrollbar',
+    scrollbarHide: true,
+    slidesPerView: 'auto',
+freeMode: true,
+freeModeSticky: true,
+    centeredSlides: true,
+    spaceBetween: 30,
+    grabCursor: true
+});
---
+++ js/tailwind.config.js
@@ -0,0 +1,11 @@
+tailwind.config = {
+  darkMode: 'class',
+  theme: {
+    darkMode: 'class',
+    extend: {
+      colors: {
+        colBg: "#AC8F43",
+      },
+    },
+  },
+};
