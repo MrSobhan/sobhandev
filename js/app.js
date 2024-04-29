ed8bc0cdeffd8b7ed2eb999f7582aa8846f07290
MrSobhan <sobhan.mosazadeh85@gmail.com>
2024-04-29 23:20:28 +0330

    2024-04-29

--- js/app.js
+++ js/app.js
@@ -1,50 +1,52 @@
 const $ = document;
-AOS.init();
-
-var app = document.getElementById('app');
-
-var typewriter = new Typewriter(app, {
-    loop: true
+AOS.init({
+  easing: "ease-in-out-sine",
 });
 
-typewriter.typeString('من <سبحان /> هستم')
-    .pauseFor(2500)
-    .deleteChars(14)
-    .typeString('<فرانت اند دولوپر /> هستم')
-    .pauseFor(2500)
-    .deleteChars(26)
-    .typeString('<برنامه نویس /> هستم')
-    .pauseFor(2500)
-    .start();
+var app = document.getElementById("app");
 
-const btnDark = $.getElementById('dark')
-const btnLight = $.getElementById('light')
+var typewriter = new Typewriter(app, {
+  loop: true,
+});
 
-window.addEventListener('load' , ()=>{
-    if(localStorage.getItem('them') == 'dark'){
-        $.documentElement.classList.add('dark')
-    }else{
-        $.documentElement.classList.remove('dark')
-    }
-})
+typewriter
+  .typeString("من <سبحان /> هستم")
+  .pauseFor(2500)
+  .deleteChars(14)
+  .typeString("<فرانت اند دولوپر /> هستم")
+  .pauseFor(2500)
+  .deleteChars(26)
+  .typeString("<برنامه نویس /> هستم")
+  .pauseFor(2500)
+  .start();
 
+const btnDark = $.getElementById("dark");
+const btnLight = $.getElementById("light");
 
-btnDark.addEventListener('click' , ()=>{
-    localStorage.setItem('them' , 'dark')
-    $.documentElement.classList.add('dark')
-})
-btnLight.addEventListener('click' , ()=>{
-    localStorage.setItem('them' , 'light')
-    $.documentElement.classList.remove('dark')
-})
+window.addEventListener("load", () => {
+  if (localStorage.getItem("them") == "dark") {
+    $.documentElement.classList.add("dark");
+  } else {
+    $.documentElement.classList.remove("dark");
+  }
+});
 
-var swiper = new Swiper('.swiper-container', {
-    scrollbar: '.swiper-scrollbar',
-    scrollbarHide: true,
-    slidesPerView: 'auto',
-freeMode: true,
-freeModeSticky: true,
-    centeredSlides: true,
-    spaceBetween: 30,
-    grabCursor: true
+btnDark.addEventListener("click", () => {
+  localStorage.setItem("them", "dark");
+  $.documentElement.classList.add("dark");
+});
+btnLight.addEventListener("click", () => {
+  localStorage.setItem("them", "light");
+  $.documentElement.classList.remove("dark");
+});
+
+var swiper = new Swiper(".swiper-container", {
+  scrollbar: ".swiper-scrollbar",
+  scrollbarHide: true,
+  slidesPerView: "auto",
+  freeMode: true,
+  freeModeSticky: true,
+  centeredSlides: true,
+  spaceBetween: 30,
+  grabCursor: true,
 });
